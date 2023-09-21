1bff9b8673d5a5dcccf21fb47ed6af9e5fe48a5f
alexandredchaves <xandechaves@gmail.com>
2023-09-21 10:50:20 -0300

    corrigindo arquivos JS

--- JS/script-carrinho.js
+++ JS/script-carrinho.js
@@ -1,10 +1,12 @@
-let modal = document.querySelector(".produtos-carrinho");
-let excluir = document.querySelector(".bi-trash");
+let modal = document.querySelectorAll(".produtos-carrinho");
+let excluir = document.querySelectorAll(".bi-trash");
 
-for(let i = 0; i < excluir.length; i++){
+for(let i =0; i < excluir.length; i++){
+    // for(let i =0; i < modal.length; i++){
     excluir[i].addEventListener("click", function(event){
         if(!event.target.classList.contains(".bi-trash")){
             modal[i].remove();
-        };
+        }
     });
 };
+// };
--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,39 @@
-let btnAdd = document.querySelector("#addBotao");
-btnAdd.addEventListener("click", function(){
-    let inputValor = document.querySelector("#meuInput").value;
-    if(inputValor == ""){
-        alert("Preencha o campo");
-    }else{
-        alert("Login realizado com sucesso");
-    }
+const btnAdd = document.querySelector("#addBtn"); // Pega o botão
 
-    document.querySelector("#meuInput").value = "";
-})
+// Cria um evento de click para o botão, que irá executar a function anonima:
+btnAdd.addEventListener("click", function () {
+   // Verifica se o input está vazio 
+  const inputValue = document.querySelector("#myInput").value;
+  if (inputValue === "") {
+    alert("Você deve digitar algum texto para adicionar!");
+  } else {
+    //alert("LOGIN REALIZADO COM SUCESSO");
+    // document.getElementById("myUL").appendChild(li); // Add a li na ul
+    document.querySelector("#mensagem-ok").insertAdjacentHTML(
+        "afterend",
+        `
+      <div class="card-teste">
+              <span>LOGIN EFETUADO COM SUCESSO</span>
+      </div>
+      `
+     
+      );  
+      
+      const mensagem = document.querySelector('.card-teste');
+      // closeMessage.addEventListener("click", function (){
+      //   mensagem.style.display = "none";
+      // });
+      
+      setTimeout(() => {
+        mensagem.style.display = "none";
+      }, 3000);
+  }
+  
+  //comando para zerar o input
+  document.querySelector("#myInput").value = ""; // Zera o input
+  });
+
+ 
+
+
+ 
